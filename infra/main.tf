e533280ac82aafd9936822cc2a0e17bffd370a18
chenaoh <juan.leviathan@outlook.com>
2025-03-11 22:41:01 -0500

    ver1.0

---
+++ infra/main.tf
@@ -0,0 +1,33 @@
+provider "google" {
+  credentials = file("gcp-key.json")  # Usa la clave JSON de la cuenta de servicio
+  project     = var.project_id
+  region      = "us-central1"
+}
+
+variable "project_id" {
+  default = "spiderops"
+}
+
+resource "google_container_cluster" "primary" {
+  name     = "my-gke-cluster"
+  location = "us-central1-a"
+
+  remove_default_node_pool = true
+  initial_node_count       = 1
+
+  node_config {
+    machine_type = "e2-medium"
+    disk_size_gb = 20
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
+  }
+}
+
+resource "google_artifact_registry_repository" "repo" {
+  provider      = google
+  project       = var.project_id
+  location      = "us-central1"
+  repository_id = "my-repo"
+  format        = "DOCKER"
+}
